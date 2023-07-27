d22927e0e30a1cae803c27a921fb3ff03b6831c0
cflip <cflip@cflip.net>
2023-07-26 19:49:28 -0600

    Actually use BathroomPaintings utility functions
    This class was included in a previous commit but was never actually used
    anywhere for some odd reason.

--- src/main/java/su/bathroom/BathroomMod.java
+++ src/main/java/su/bathroom/BathroomMod.java
@@ -3,16 +3,12 @@ package su.bathroom;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
-import net.minecraft.entity.decoration.painting.PaintingVariant;
 import net.minecraft.item.FoodComponent;
-import net.minecraft.item.Item;
-import net.minecraft.item.SpawnEggItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -21,19 +17,11 @@ import su.bathroom.entity.PigCreeperEntity;
 import su.bathroom.registry.BathroomBlocks;
 import su.bathroom.registry.BathroomItemGroup;
 import su.bathroom.registry.BathroomItems;
+import su.bathroom.registry.BathroomPaintings;
 
 public class BathroomMod implements ModInitializer {
 	public static final FoodComponent GUMMIES_FOOD = new FoodComponent.Builder().hunger(3).saturationModifier(0.2f).build();
 	public static final FoodComponent WORMS_IN_DIRT_FOOD = new FoodComponent.Builder().hunger(7).saturationModifier(0.4f).build();
-
-	public static final PaintingVariant MARKET = registerPainting("market", new PaintingVariant(16, 16));
-	public static final PaintingVariant EEL = registerPainting("eel", new PaintingVariant(16, 16));
-	public static final PaintingVariant DAMP = registerPainting("damp", new PaintingVariant(16, 32));
-	public static final PaintingVariant HELLPIPE = registerPainting("hellpipe", new PaintingVariant(32, 16));
-
-	private static PaintingVariant registerPainting(String name, PaintingVariant PaintingVariant) {
-		return Registry.register(Registries.PAINTING_VARIANT, new Identifier("bathroom", name), PaintingVariant);
-	}
 
 	public static EntityType<PigCreeperEntity> PIG_CREEPER_ENTITY;
 
@@ -63,7 +51,6 @@ public class BathroomMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
 		PIG_CREEPER_ENTITY = Registry.register(Registries.ENTITY_TYPE, new Identifier("bathroom", "pig_creeper"), FabricEntityTypeBuilder.create(SpawnGroup.MONSTER, PigCreeperEntity::new).dimensions(new EntityDimensions(0.6F, 1.7F, true)).trackRangeBlocks(8).build());
 		FabricDefaultAttributeRegistry.register(PIG_CREEPER_ENTITY, PigCreeperEntity.createCreeperAttributes());
 		BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.BEACH), SpawnGroup.MONSTER, PIG_CREEPER_ENTITY, 1, 12, 20);
@@ -71,5 +58,6 @@ public class BathroomMod implements ModInitializer {
 		BathroomBlocks.registerBlocks();
 		BathroomItems.registerItems();
 		BathroomItemGroup.registerItemGroup();
+		BathroomPaintings.registerPaintings();
 	}
 }
---
+++ src/main/java/su/bathroom/registry/BathroomPaintings.java
@@ -0,0 +1,19 @@
+package su.bathroom.registry;
+
+import net.minecraft.entity.decoration.painting.PaintingVariant;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class BathroomPaintings {
+	public static void registerPaintings() {
+		createPainting("damp", 16, 32);
+		createPainting("eel", 16, 16);
+		createPainting("market", 16, 16);
+		createPainting("hellpipe", 32, 16);
+	}
+
+	private static void createPainting(String id, int width, int height) {
+		Registry.register(Registries.PAINTING_VARIANT, new Identifier("bathroom", id), new PaintingVariant(width, height));
+	}
+}
