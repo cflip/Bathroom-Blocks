d899dc4fb3892aa2105411a6832950ad3e4d63f2
cflip <cflip@cflip.net>
2023-08-09 13:54:31 -0600

    Fix mushspawn item

--- src/main/java/su/bathroom/item/MushspawnItem.java
+++ src/main/java/su/bathroom/item/MushspawnItem.java
@@ -2,15 +2,18 @@ package su.bathroom.item;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraft.world.event.GameEvent;
 import su.bathroom.BathroomMod;
+import su.bathroom.entity.MushletEntity;
 
 public class MushspawnItem extends Item {
 
@@ -25,11 +28,21 @@ public class MushspawnItem extends Item {
         BlockState blockState = world.getBlockState(blockPos);
         if (blockState.isOf(Blocks.MYCELIUM)) {
             if (!world.isClient()) {
-                BathroomMod.MUSHLET_ENTITY.spawnFromItemStack((ServerWorld) world, context.getStack(), context.getPlayer(), blockPos, SpawnReason.SPAWN_EGG, true, false);
-                world.emitGameEvent(context.getPlayer(), GameEvent.ENTITY_PLACE, blockPos);
+                ServerWorld serverWorld = (ServerWorld) world;
+                ItemStack stack = context.getStack();
+                PlayerEntity player = context.getPlayer();
+
+                MushletEntity mushlet = BathroomMod.MUSHLET_ENTITY.create(serverWorld, stack.getNbt(), EntityType.copier(serverWorld, stack, player), blockPos, SpawnReason.SPAWN_EGG, true, false);
+                if (mushlet != null) {
+                    mushlet.setBaby(true);
+                    world.spawnEntity(mushlet);
+                    if (player != null && !player.getAbilities().creativeMode) {
+                        stack.decrement(1);
+                    }
+                }
             }
             return ActionResult.SUCCESS;
         }
-        return ActionResult.FAIL;
+        return ActionResult.PASS;
     }
 }
