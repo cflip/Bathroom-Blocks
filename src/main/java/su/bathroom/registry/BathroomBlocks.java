8af68662775fec853a1effc785d3bf6bf065121f
Kjolt947 <67553897+Kjolt947@users.noreply.github.com>
2023-10-10 16:41:56 -0600

    new tiles

--- src/main/java/su/bathroom/registry/BathroomBlocks.java
+++ src/main/java/su/bathroom/registry/BathroomBlocks.java
@@ -14,14 +14,20 @@ import net.minecraft.util.Identifier;
 
 public class BathroomBlocks {
 	public static final Block BATHROOM_TILES = new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK).slipperiness(0.7f).requiresTool().strength(1.5F, 6.0F));
-	public static final Block RAW_BATHROOM = new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK).slipperiness(0.7f).requiresTool().strength(1.5F, 6.0F));
+	public static final Block RAW_BATHROOM = new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK).slipperiness(0.5f).requiresTool().strength(1.5F, 6.0F));
 	public static final Block SLIPTILES = new Block(FabricBlockSettings.create().slipperiness(0.7f).requiresTool().strength(1.5F, 6.0F).sounds(BlockSoundGroup.METAL));
 	public static final Block DARK_SLIPTILES = new Block(FabricBlockSettings.create().slipperiness(0.7f).requiresTool().strength(1.5F, 6.0F).sounds(BlockSoundGroup.METAL));
 	public static final Block MIXED_SLIPTILES = new Block(FabricBlockSettings.create().slipperiness(0.7f).requiresTool().strength(1.5F, 6.0F).sounds(BlockSoundGroup.METAL));
 	public static final Block SLIPBRICKS = new Block(FabricBlockSettings.create().slipperiness(0.7f).requiresTool().strength(1.5F, 6.0F).sounds(BlockSoundGroup.METAL));
 	public static final Block DARK_SLIPBRICKS = new Block(FabricBlockSettings.create().slipperiness(0.7f).requiresTool().strength(1.5F, 6.0F).sounds(BlockSoundGroup.METAL));
-	public static final Block MIXED_BRICKS = new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK).requiresTool().strength(2F, 6.0F));
-	public static final SlabBlock MIXED_BRICKS_SLAB = new SlabBlock(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK).requiresTool().strength(2F, 6.0F));
+	public static final Block FRUITY_TILES = new Block(FabricBlockSettings.create().slipperiness(0.7f).requiresTool().strength(1.2F, 6.0F).sounds(BlockSoundGroup.METAL));
+	public static final Block BERRY_TILES = new Block(FabricBlockSettings.create().slipperiness(0.7f).requiresTool().strength(1.2F, 6.0F).sounds(BlockSoundGroup.METAL));
+	public static final Block MELON_TILES = new Block(FabricBlockSettings.create().slipperiness(0.7f).requiresTool().strength(1.2F, 6.0F).sounds(BlockSoundGroup.METAL));
+	public static final Block SOUR_TILES = new Block(FabricBlockSettings.create().slipperiness(0.7f).requiresTool().strength(1.2F, 6.0F).sounds(BlockSoundGroup.METAL));
+	public static final Block TURTLE_TILES = new Block(FabricBlockSettings.create().slipperiness(0.7f).requiresTool().strength(4F, 10.0F).sounds(BlockSoundGroup.STONE));
+	public static final Block TORTOISE_TILES = new Block(FabricBlockSettings.create().requiresTool().strength(4F, 10.0F).sounds(BlockSoundGroup.STONE));
+	public static final Block MIXED_BRICKS = new Block(FabricBlockSettings.copyOf(Blocks.BRICKS).requiresTool().strength(2F, 6.0F));
+	public static final SlabBlock MIXED_BRICKS_SLAB = new SlabBlock(FabricBlockSettings.copyOf(Blocks.BRICKS).requiresTool().strength(2F, 6.0F));
 	public static final Block INKY_COBBLESTONE = new Block(FabricBlockSettings.copyOf(Blocks.STONE).requiresTool().strength(2F, 6.0F));
 	public static final SlabBlock INKY_COBBLESTONE_SLAB = new SlabBlock(FabricBlockSettings.copyOf(Blocks.STONE).requiresTool().strength(2F, 6.0F));
 	public static final Block INKY_MOSS_STONE = new Block(FabricBlockSettings.copyOf(Blocks.STONE).requiresTool().strength(2F, 6.0F));
@@ -36,6 +42,12 @@ public class BathroomBlocks {
 		registerBlockWithItem("mixed_sliptiles", MIXED_SLIPTILES);
 		registerBlockWithItem("slipbricks", SLIPBRICKS);
 		registerBlockWithItem("dark_slipbricks", DARK_SLIPBRICKS);
+		registerBlockWithItem("fruity_tiles", FRUITY_TILES);
+		registerBlockWithItem("berry_tiles", BERRY_TILES);
+		registerBlockWithItem("melon_tiles", MELON_TILES);
+		registerBlockWithItem("sour_tiles", SOUR_TILES);
+		registerBlockWithItem("turtle_tiles", TURTLE_TILES);
+		registerBlockWithItem("tortoise_tiles", TORTOISE_TILES);
 		registerBlockWithItem("mixed_bricks", MIXED_BRICKS);
 		registerBlockWithItem("mixed_bricks_slab", MIXED_BRICKS_SLAB);
 		registerBlockWithItem("inky_cobblestone", INKY_COBBLESTONE);
--- src/main/java/su/bathroom/registry/BathroomItemGroup.java
+++ src/main/java/su/bathroom/registry/BathroomItemGroup.java
@@ -19,6 +19,12 @@ public class BathroomItemGroup {
 				entries.add(BathroomBlocks.MIXED_SLIPTILES);
 				entries.add(BathroomBlocks.SLIPBRICKS);
 				entries.add(BathroomBlocks.DARK_SLIPBRICKS);
+				entries.add(BathroomBlocks.FRUITY_TILES);
+				entries.add(BathroomBlocks.BERRY_TILES);
+				entries.add(BathroomBlocks.MELON_TILES);
+				entries.add(BathroomBlocks.SOUR_TILES);
+				entries.add(BathroomBlocks.TURTLE_TILES);
+				entries.add(BathroomBlocks.TORTOISE_TILES);
 				entries.add(BathroomBlocks.MIXED_BRICKS);
 				entries.add(BathroomBlocks.MIXED_BRICKS_SLAB);
 				entries.add(BathroomBlocks.INSULATION);
